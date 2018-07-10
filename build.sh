2f9e1b2cfc1b1b6e12be759523faa4be71693698
shahela-quader <quader.shahela@gmail.com>
2018-07-09 21:58:33 -0400

    Update build.sh
    replaced my ecs url

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker build -t tetranoodle:v_$BUILD_NUMBER --pull=true /var/lib/jenkins/workspace/tetranoodle-app \
-&& docker tag tetranoodle:v_$BUILD_NUMBER 133976391764.dkr.ecr.us-east-1.amazonaws.com/tetranoodle:v_$BUILD_NUMBER \
-&& docker push 133976391764.dkr.ecr.us-east-1.amazonaws.com/tetranoodle:v_$BUILD_NUMBER
+&& docker tag tetranoodle:v_$BUILD_NUMBER 946553220451.dkr.ecr.us-east-1.amazonaws.com/tetranoodle:v_$BUILD_NUMBER \
+&& docker push 946553220451.dkr.ecr.us-east-1.amazonaws.com/tetranoodle:v_$BUILD_NUMBER
 
 
 
